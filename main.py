587602fc8ea095651db6320027d7a912f9a2498b
unknown <mjschuette4@gmail.com>
2020-08-26 18:26:44 -0500

    Got window displaying

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+import sys
+from game_window_class import *
+
+WIDTH, HEIGHT = 800, 800
+BACKGROUND = (199, 199, 199)
+FPS = 60
+
+def get_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+def update():
+    game_window.update()
+
+def draw():
+    window.fill(BACKGROUND)
+    game_window.draw()
+
+pygame.init()
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+game_window = Game_window(window, 100, 180)
+
+running = True
+while running:
+    get_events()
+    update()
+    draw()
+    pygame.display.update()
+    clock.tick(FPS)
+pygame.quit()
+sys.exit()
