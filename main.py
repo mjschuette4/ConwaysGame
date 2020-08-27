69317a395882160e9de8aef038fa3b761680e950
unknown <mjschuette4@gmail.com>
2020-08-27 13:33:37 -0500

    Can click on cells to choose which you want alive

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def get_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if mouse_on_grid(mouse_pos):
+                click_cell(mouse_pos)
 
 def update():
     game_window.update()
@@ -17,6 +21,18 @@ def update():
 def draw():
     window.fill(BACKGROUND)
     game_window.draw()
+
+def mouse_on_grid(pos):
+    if pos[0] > 100 and pos[0] < WIDTH-100:
+        if pos[1] > 180 and pos[1] < HEIGHT-20:
+            return True
+    return False
+
+def click_cell(pos):
+    grid_pos = [pos[0]-100, pos[1]-180]
+    grid_pos[0] = grid_pos[0]//20
+    grid_pos[1] = grid_pos[1]//20
+    game_window.grid[grid_pos[1]][grid_pos[0]].alive = True
 
 pygame.init()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
