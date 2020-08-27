9b1affa2bf6fb267eed23c6ecda2dd23e605e60f
unknown <mjschuette4@gmail.com>
2020-08-27 18:46:23 -0500

    Game works, Still not quite at MVP

--- cell_class.py
+++ cell_class.py
@@ -9,9 +9,13 @@ class Cell:
         self.image = pygame.Surface((20, 20))
         self.rect = self.image.get_rect()
         self.neighbors = []
+        self.alive_neighbors = 0
 
     def update(self):
         self.rect.topleft = (self.grid_x*20, self.grid_y*20)
+        for cell in self.neighbors:
+            if cell.alive:
+                self.alive_neighbors += 1
 
     def draw(self):
         if self.alive:
@@ -22,7 +26,7 @@ class Cell:
         self.surface.blit(self.image, (self.grid_x*20, self.grid_y*20))
 
     def get_neighbors(self, grid):
-        neighbor_list = [[1,1],[-1,-1],[1,-1],[0,-1],[1,0],[-1,0]]
+        neighbor_list = [[1,1],[-1,-1],[-1,1],[1,-1],[0,-1],[0,1],[1,0],[1,0],[-1,0]]
         for neighbor in neighbor_list:
             neighbor[0] += self.grid_x
             neighbor[1] += self.grid_y
@@ -40,6 +44,15 @@ class Cell:
                 self.neighbors.append(grid[neighbor[1]][neighbor[0]])
             except:
                 print(neighbor)
+
+    def live_neighbors(self):
+        count = 0
+        for neighbor in self.neighbors:
+            if neighbor.alive:
+                count += 1
+        
+        self.alive_neighbors = count
+        
                 
 
 
--- game_window_class.py
+++ game_window_class.py
@@ -1,4 +1,5 @@
 import pygame
+import copy
 from cell_class import *
 vec = pygame.math.Vector2
 
@@ -28,3 +29,27 @@ class Game_window:
             for cell in row:
                 cell.draw()
         self.screen.blit(self.image, (self.pos.x, self.pos.y))
+
+    def reset_grid(self):
+        self.grid = [[Cell(self.image, x, y) for x in range(self.cols)] for y in range(self.rows)]
+   
+    def evaluate(self):
+        new_grid = copy.copy(self.grid)
+        for row in self.grid:
+            for cell in row:
+                cell.live_neighbors()
+
+        for yidx, row in enumerate(self.grid):
+            for xidx, cell in enumerate(row):
+                if cell.alive:
+                    if cell.alive_neighbors == 2 or cell.alive_neighbors == 3:
+                        new_grid[yidx][xidx].alive = True
+                    if cell.alive_neighbors < 2:
+                        new_grid[yidx][xidx].alive = False
+                    if cell.alive_neighbors > 3:
+                        new_grid[yidx][xidx].alive = False
+                else:
+                    if cell.alive_neighbors == 3:
+                        new_grid[yidx][xidx].alive = True
+        self.grid = new_grid
+
