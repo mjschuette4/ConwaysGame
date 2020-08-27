f241693f644df4a67763dbfff872e534431f3d08
unknown <mjschuette4@gmail.com>
2020-08-27 13:51:32 -0500

    Detecting neighbors

--- cell_class.py
+++ cell_class.py
@@ -8,6 +8,7 @@ class Cell:
         self.grid_y = grid_y
         self.image = pygame.Surface((20, 20))
         self.rect = self.image.get_rect()
+        self.neighbors = []
 
     def update(self):
         self.rect.topleft = (self.grid_x*20, self.grid_y*20)
@@ -19,3 +20,26 @@ class Cell:
             self.image.fill((0, 0, 0))
             pygame.draw.rect(self.image, (255, 255, 255), (1, 1, 16, 16))
         self.surface.blit(self.image, (self.grid_x*20, self.grid_y*20))
+
+    def get_neighbors(self, grid):
+        neighbor_list = [[1,1],[-1,-1],[1,-1],[0,-1],[1,0],[-1,0]]
+        for neighbor in neighbor_list:
+            neighbor[0] += self.grid_x
+            neighbor[1] += self.grid_y
+        for neighbor in neighbor_list:
+            if neighbor[0] < 0:
+                neighbor[0] += 30
+            if neighbor[1] < 0:
+                neighbor[1] += 30
+            if neighbor[1] > 29:
+                neighbor[1] -= 30
+            if neighbor[0] > 29:
+                neighbor[0] -= 30
+        for neighbor in neighbor_list:
+            try:
+                self.neighbors.append(grid[neighbor[1]][neighbor[0]])
+            except:
+                print(neighbor)
+                
+
+
